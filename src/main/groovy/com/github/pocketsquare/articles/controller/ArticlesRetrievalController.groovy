a4235deea7ec12341b9bb6cd69f9873cd775c773
Yaroslav Yermilov <yermilov17@gmail.com>
2017-03-21 09:23:16 +0200

    sort by time added

--- src/main/groovy/com/github/pocketsquare/articles/controller/ArticlesRetrievalController.groovy
+++ src/main/groovy/com/github/pocketsquare/articles/controller/ArticlesRetrievalController.groovy
@@ -5,6 +5,7 @@ import com.github.pocketsquare.articles.repository.ArticleRepository
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
@@ -20,6 +21,6 @@ class ArticlesRetrievalController {
 
     @GetMapping(path = '/article/byUserId/{userId}/unread')
     @ResponseBody Collection<Article> getUnreadByUserId(@PathVariable String userId, @RequestParam('page') Integer page, @RequestParam('size') Integer size) {
-        articleRepository.findByUserIdAndRead(userId, false, new PageRequest(page, size))
+        articleRepository.findByUserIdAndRead(userId, false, new PageRequest(page, size, new Sort(Sort.Direction.DESC, 'timeAdded')))
     }
 }
