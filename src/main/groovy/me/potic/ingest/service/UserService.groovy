86dbf634bc824bbc6668f9f64b6498c212cac245
Yaroslav Yermilov <yermilov17@gmail.com>
2017-10-04 01:24:20 +0300

    #2. use new user api

--- src/main/groovy/me/potic/ingest/service/UserService.groovy
+++ src/main/groovy/me/potic/ingest/service/UserService.groovy
@@ -1,7 +1,6 @@
 package me.potic.ingest.service
 
 import com.codahale.metrics.annotation.Timed
-import groovy.json.JsonSlurper
 import groovy.util.logging.Slf4j
 import groovyx.net.http.HttpBuilder
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,30 +11,26 @@ import org.springframework.stereotype.Service
 @Slf4j
 class UserService {
 
-    HttpBuilder userServiceRest
+    HttpBuilder usersServiceRest
 
     @Autowired
-    JsonSlurper jsonSlurper
-
-    @Autowired
-    HttpBuilder userServiceRest(@Value('${services.userService.url}') String userServiceUrl) {
-        userServiceRest = HttpBuilder.configure {
-            request.uri = userServiceUrl
+    HttpBuilder usersServiceRest(@Value('${services.users.url}') String usersServiceUrl) {
+        usersServiceRest = HttpBuilder.configure {
+            request.uri = usersServiceUrl
         }
     }
 
     @Timed(name = 'allUsersIds')
     Collection<String> allUsersIds() {
-        byte[] response = userServiceRest.get {
-            request.uri.path = '/user'
-        }
+        log.info 'fetching all users ids'
 
-        // temporary fix as response is byte array for some reason
-        def jsonResponse = jsonSlurper.parse(response)
-
-        jsonResponse['_embedded']['user'].collect { def user ->
-            String userHref = user['_links']['self']['href']
-            return userHref.substring(userHref.lastIndexOf('/') + 1)
+        try {
+            return usersServiceRest.get {
+                request.uri.path = '/users/ids'
+            }
+        } catch (e) {
+            log.error "fetching all users ids failed: $e.message", e
+            throw new RuntimeException('fetching all users ids failed', e)
         }
     }
 }
